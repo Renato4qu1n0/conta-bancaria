b4fb59d8e0d2feabcb4327d43f8a6b5d6a0444f7
Renato Aquino <re4qu1n0@gmail.com>
2025-04-24 21:12:23 -0300

    Projeto completo do desafio da DIO criando uma conta corrente no terminal.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite o seu nome Completo: ");
+        var nomeCliente = entrada.nextLine();
+
+        System.out.print("Digite o número da Agência: ");
+        var agencia = entrada.nextLine();
+
+        System.out.print("Agora digite o número da sua conta: ");
+        int numeroConta = entrada.nextInt();
+
+        double saldo = 2525.00;
+
+        System.out.println("Seja bem-vindo " + nomeCliente + "!" + ". O número da sua Agência é: " + agencia +
+                ". A sua conta é a de número: " + numeroConta + ". Você tem disponível em sua conta o valor de: R$" + saldo);
+
+        System.out.println("Qual valor o Sr gostaria de sacar hoje?");
+        double saque = entrada.nextDouble();
+
+        double valorFinal = saldo - saque;
+
+        System.out.println("Você sacou o valor de: R$" + saque);
+        System.out.println("Agora você tem disponível o valor dê R$" + valorFinal);
+
     }
 }
